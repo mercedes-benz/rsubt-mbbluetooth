e694cd810fd5786c33ecc38f83699c5e86ca3ac6
Frederic Bedau <Frederic.Bedau@mercedes-benz.com>
2026-07-09 13:43:32 +0200

    mb_bt.mk: enable only SOURCE_SBC and SOURCE_AAC
    Enable support only for SOURCE_SBC and SOURCE_AAC codec.
    Specifically disable aptX codec.
    
    Issue-ID: STARC 23236668
    
    Cherry-picked commit: 1d48bea5552a1368895ef76ba32dc8b87def0300

--- mb_bt.mk
+++ mb_bt.mk
@@ -2,6 +2,7 @@
 # Bluetooth A2DP properties
 
 PRODUCT_PRODUCT_PROPERTIES += persist.bluetooth.bluetooth_audio_hal.disabled=true
+PRODUCT_PRODUCT_PROPERTIES += persist.bt.a2dp.codec.support=3
 
 MB_BT_SCRIPT := $(PWD)/vendor/mercedes/packages/MBBluetooth/mb_bt.sh
 $(shell chmod 777 $(MB_BT_SCRIPT))
